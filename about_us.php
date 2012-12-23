1062c1c63c0648cae84a94752da4cd81a21b79cb
Alfonza Lewis <alfonza.lewis@gatech.edu>
2012-12-23 18:31:07 -0500

    I am adding the files that were previously worked on.
    The About Us file contains information about this organization. The
    Header html file is the top portion of each webpage. The layout file
    describes the look and style of each webpage.

---
+++ about_us.php
@@ -0,0 +1,28 @@
+<?php
+include("header.html");
+?>
+
+<div id="pageContent">
+	<h2>About Us</h2>
+	<p>
+		The AUC Alliance is an organization that aims to help LGBT students identify and develop strategies for managing and 
+		leveraging their identify in the workplace. Additionally the organization aims to create a dynamic network between LGBTQA
+		students and LGBTQA professionals in industry and academia.
+	</p>
+	<p>The organization is affiliated with the following national organizations:</p>
+		<ol>
+			<li>
+				oSTEM (Out in Science, Technology, Engineering, and Mathematics): a national LGBTQ-affirming organization that
+				aims to increase participation and provide services/support for LGBTQA students in science, technology,
+				engineering, and mathematics
+			</li>
+			<li>
+				OUBC (the Out for Undergraduate Business Conference): an organization dedicated to educating, inspiring, and
+				placing LGBT students in financial services and consulting positions.
+			</li>
+		</ol>
+</div>
+</body>
+</html>
+		
+
